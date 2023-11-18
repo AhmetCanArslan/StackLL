00b27f6e620c42f1fae252de8ada55db632966a1
AhmetCanArslan <ahmetcan.arslan21@ogr.atauni.edu.tr>
2023-11-18 20:48:29 +0300

    Files uploaded

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿
+public class Node
+{
+    public int data;
+    public Node next;
+    public Node(int data)
+    {
+        this.data = data;
+        this.next = null;
+    }
+}
+public class Stack
+{
+    private Node ust;
+    public Stack() { ust = null; }
+
+    public void Push(int data)
+    {
+        Node newNode = new Node(data);
+        newNode.next = ust;
+        ust = newNode;
+    }
+
+    public void Pop()
+    {
+        if (ust == null)
+        { 
+            Console.WriteLine("Stack is empty");
+            return;
+        }
+        ust = ust.next;
+    }
+    public int Peek()
+    {
+        if (ust == null)
+            return -1;
+        return ust.data;
+    }
+}
+
+class Hello
+{
+    public static void Main()
+    {
+        Stack stack = new Stack();
+        stack.Push(10);
+        stack.Push(21);
+        stack.Push(90);
+        Console.WriteLine(stack.Peek());
+        stack.Pop();
+        stack.Pop();
+        Console.WriteLine(stack.Peek());
+        
+    }
+}
